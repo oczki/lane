939c53b671426b576f445f607d0758eb488e6b7c
Marcin Kurczewski <mkurczew@gmail.com>
2014-03-08 23:05:09 +0100

    Html - IDs changed to classes

--- public_html/js/list.js
+++ public_html/js/list.js
@@ -9,13 +9,13 @@ $(function()
 		queue.delayedFlush();
 	});
 
-	$('#add-new-list, #menu-login, #menu-register').click(function(e)
+	$('#list-management .add-list, #menu .login, #menu .register').click(function(e)
 	{
 		e.preventDefault();
 		showPopup($(this).attr('href'));
 	});
 
-	$('#menu-logout').click(function(e)
+	$('#menu .logout').click(function(e)
 	{
 		e.preventDefault();
 		$.post($(this).attr('href'), function()
