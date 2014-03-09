4c11e4b52dba250794498782499e52b1d2fa4954
Marcin Kurczewski <mkurczew@gmail.com>
2014-03-09 11:35:34 +0100

    Queue code moved to separate file

---
+++ public_html/js/queue.js
@@ -0,0 +1,71 @@
+function Queue()
+{
+	var q = this;
+
+	q.jobs = [];
+	q.interval = false;
+	q.flushing = false;
+	q.disabled = false;
+
+	q.push = function(job)
+	{
+		$('#save-info').text('Saving...');
+		enableExitConfirmation('Changes were not saved!');
+		q.jobs.push(job);
+	}
+
+	q.flush = function(callback)
+	{
+		if (q.disabled)
+		{
+			return;
+		}
+
+		if (q.flushing)
+		{
+			window.setTimeout(q.flush, 500);
+			return;
+		}
+
+		q.flushing = true;
+
+		var url = '/exec';
+		var data = {jobs: q.jobs};
+		$.post(url, data, function(rawContent)
+		{
+			var content = $(rawContent);
+
+			//check for any errors, if errors were found - disable all editing
+			if (content.find('.error').length > 0)
+			{
+				alert("An error occured:\n\n" +
+					content.find('.error').text() +
+					"\n\nPlease reload the page and redo the changes.");
+				q.disabled = true;
+			}
+
+			if (typeof(callback) !== 'undefined')
+				callback();
+
+			disableExitConfirmation();
+			$('#save-info').text('Saved');
+
+			q.flushing = false;
+			q.jobs = [];
+		});
+	}
+
+	q.delayedFlush = function()
+	{
+		if (q.interval)
+			window.clearTimeout(q.interval);
+
+		q.interval = window.setTimeout(q.flush, 1000);
+	}
+}
+
+function Job(name, args)
+{
+	this.name = name;
+	this.args = args;
+}
